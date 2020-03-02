ecbf3d2323087b1d53571c8950eb1ae0ede8d419
Sara Gorecki <sgorecki@gmail.com>
2020-03-02 14:55:28 -0500

    Add basic user model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
+
+    def _repr_(self):
+        return f'<User {self.username}>'
