ef26790fd312c71137742cff7ed01ee24a6d60d3
Sara Gorecki <sgorecki@gmail.com>
2020-03-09 16:40:09 -0400

    Add to config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://localhost/reading_list'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CSRF_ENABLED = True
