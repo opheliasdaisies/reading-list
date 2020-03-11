6277415db04691c026afa305469c480f7fc97d14
Sara Gorecki <sgorecki@gmail.com>
2020-03-11 15:50:32 -0400

    Add route to update users

--- app/routes.py
+++ app/routes.py
@@ -41,8 +41,41 @@ def get_user(id):
     return jsonify(deserialized_data), 200
 
 @app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id, data):
-    pass
+def update_user(id):
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    data_to_validate = {}
+
+    try:
+        if 'password' in data:
+            if not user.check_password(data['password']):
+                raise ValidationError('Invalid entry')
+            if 'new_password' in data and 'new_password2' in data and data['new_password'] == data['new_password2']:
+                data_to_validate['password'] = data['new_password']
+            else:
+                raise ValidationError('New passwords do not match', 'new_password')
+
+        if 'email' in data and data['email'] != user.email:
+            data_to_validate['email'] = data['email']
+        if 'username' in data and data['username'] != user.username:
+            data_to_validate['username'] = data['username']
+
+        errors = user_schema.validate(data_to_validate, partial=True)
+        if errors:
+            db.session.rollback()
+            raise ValidationError(errors)
+
+        for field in data_to_validate:
+            if field is 'password':
+                user.set_password(data['new_password'])
+            else:
+                setattr(user, field, data_to_validate.get(field))
+
+        db.session.commit()
+        return jsonify(user_schema.dump(user)), 200
+    except ValidationError as err:
+        db.session.rollback()
+        return jsonify(err.messages), 400
 
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
